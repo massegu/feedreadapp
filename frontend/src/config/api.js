5fc4e1459f47bde7e0d4b2b317aa68558a26e7ba
massegu <massegu@gmail.com>
2025-10-28 10:05:29 +0000

    añadido actualizacion codespace URKL

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
-// src/config/api.js
-const API_BASE_URL =
+
+const API_BASE_URL = 
   process.env.REACT_APP_API_BASE_URL ||
-  "https://musical-goggles-4wr7prr9qjh7qr4-8000.app.github.dev"; // ← tu URL actual en Codespaces
+  `https://${window.location.hostname.replace("3000", "8000")}`;
 
 export default API_BASE_URL;
