88e6cde13b6305f2666502bf89f808dac59dec7a
massegu <massegu@gmail.com>
2025-10-28 09:23:08 +0000

    añadido api.js

---
+++ frontend/src/config/api.js
@@ -0,0 +1,6 @@
+// src/config/api.js
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://musical-goggles-4wr7prr9qjh7qr4-8000.app.github.dev"; // ← tu URL actual en Codespaces
+
+export default API_BASE_URL;
