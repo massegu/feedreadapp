148c36b4293172cb1c41be12587be921d1f9b163
massegu <massegu@gmail.com>
2025-10-31 11:04:38 +0000

    añadido puerto 5000

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 
 const API_BASE_URL = 
   process.env.REACT_APP_API_BASE_URL ||
-  `https://${window.location.hostname.replace("3000", "8000")}`;
+  `https://${window.location.hostname.replace("3000", "8000","5000")}`;
 
 export default API_BASE_URL;
